51adea79a56f427208ea28755ba4ba7fea6b0799
moelmou <mohamedelmouki0@gmail.com>
2022-06-26 09:41:43 +0100

    contact modified

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -22,26 +22,27 @@ const Contact = () => {
 
       <div className="container contact__container">
         <div className="contact__options">
-        <article className="contact__option">
-            <MdOutlineEmail className='contact__option-icon'/>
-            <h4>Email</h4>
-            <h5>mohamedelmouki0@gmail</h5>
-            <a href="mailto:mohamedelmouki0@gmail.com" target='_blank'>Send a message</a>
-          </article>
           <article className="contact__option">
-            <RiMessengerLine className='contact__option-icon'/>
-            <h4>Facebook</h4>
-            <h5>mohamed el mouki</h5>
-            <a href="https://m.me/mohamedelmouki0" target='_blank'>Send a message</a>
-          </article>
-          <article className="contact__option">
-            <BsWhatsapp className='contact__option-icon'/>
-            <h4>WhatsApp</h4>
-            <h5>+212632007841</h5>
-            <a href="https://api.whatsapp.com/send?phone=00212632007841" target='_blank'>Send a message</a>
-          </article>
+              <MdOutlineEmail className='contact__option-icon'/>
+              <h4>Email</h4>
+              <h5>mohamedelmouki0@gmail</h5>
+              <a href="mailto:mohamedelmouki0@gmail.com" target='_blank'>Send a message</a>
+            </article>
+            <article className="contact__option">
+              <RiMessengerLine className='contact__option-icon'/>
+              <h4>Facebook</h4>
+              <h5>mohamed el mouki</h5>
+              <a href="https://m.me/mohamedelmouki0" target='_blank'>Send a message</a>
+            </article>
+            <article className="contact__option">
+              <BsWhatsapp className='contact__option-icon'/>
+              <h4>WhatsApp</h4>
+              <h5>+212632007841</h5>
+              <a href="https://api.whatsapp.com/send?phone=00212632007841" target='_blank'>Send a message</a>
+            </article>
         </div>
         <form ref={form}  onSubmit={sendEmail}>
+          <legend>Contact Me Directly</legend>
           <input type="text" name='name' placeholder='Your full name' required />
           <input type="email" name="email" placeholder='Your email' required  />
           <textarea name="message" id="" rows="7" placeholder='Your message' required ></textarea>
