2cefbe43a522912a8b4ab8d5082df6aec1da3922
moelmou <mohamedelmouki0@gmail.com>
2022-06-28 13:14:49 +0100

    modif v1.1.2

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
+import {FaTwitterSquare} from 'react-icons/fa'
+
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials' >
         <a href="https://www.linkedin.com/in/elmoukimohamed/" target='_blank'><BsLinkedin/></a>
-        <a href="https://github.com/moelmou/" target='_blank'><FaGithub/></a>
+        <a href="https://github.com/moelmou/" target='_blank'><FaGithub/></a> 
+        <a href="https://github.com/moelmou/" target='_blank'><FaTwitterSquare/></a> 
     </div>
   )
 }
